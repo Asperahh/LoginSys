40bd5a75934737cda0fb0fcca76b6c4d39098a95
Asperahh <metatoda330@gmail.com>
2025-11-18 13:17:13 +0800

    Bored and made this to learn COMP2011

---
+++ Test1.cpp
@@ -0,0 +1,269 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 21;
+
+int AccountNo = 0;
+
+
+
+
+
+struct Account{
+    char Name[N];
+    char password[N];
+    int Id;
+    bool Log = false;
+
+    Account(const char* n, const char* p, int id) {
+        strncpy(Name, n, 21);
+        Name[20] = '\0'; 
+        strncpy(password, p, 21);
+        password[20] = '\0';
+        Id = id;
+    
+    }
+};
+
+struct AccountNode{
+    Account* data;
+    AccountNode* next;
+    AccountNode* prev;
+
+    AccountNode(Account* ac, AccountNode* nxt = nullptr, AccountNode* prv = nullptr)
+        : data(ac), next(nxt), prev(prv) {}
+};
+
+
+
+void RHelp(AccountNode* temp);
+
+AccountNode* head = nullptr;
+Account* Session = nullptr;
+
+void print(int k){
+    switch(k){
+        case 1:
+            cout<<R"(          _____                    _____                _____                   _______         
+         /\    \                  /\    \              /\    \                 /::\    \        
+        /::\____\                /::\    \            /::\    \               /::::\    \       
+       /::::|   |               /::::\    \           \:::\    \             /::::::\    \      
+      /:::::|   |              /::::::\    \           \:::\    \           /::::::::\    \     
+     /::::::|   |             /:::/\:::\    \           \:::\    \         /:::/~~\:::\    \    
+    /:::/|::|   |            /:::/__\:::\    \           \:::\    \       /:::/    \:::\    \   
+   /:::/ |::|   |           /::::\   \:::\    \          /::::\    \     /:::/    / \:::\    \  
+  /:::/  |::|   | _____    /::::::\   \:::\    \        /::::::\    \   /:::/____/   \:::\____\ 
+ /:::/   |::|   |/\    \  /:::/\:::\   \:::\    \      /:::/\:::\    \ |:::|    |     |:::|    |
+/:: /    |::|   /::\____\/:::/  \:::\   \:::\____\    /:::/  \:::\____\|:::|____|     |:::|    |
+\::/    /|::|  /:::/    /\::/    \:::\  /:::/    /   /:::/    \::/    / \:::\    \   /:::/    / 
+ \/____/ |::| /:::/    /  \/____/ \:::\/:::/    /   /:::/    / \/____/   \:::\    \ /:::/    /  
+         |::|/:::/    /            \::::::/    /   /:::/    /             \:::\    /:::/    /   
+         |::::::/    /              \::::/    /   /:::/    /               \:::\__/:::/    /    
+         |:::::/    /               /:::/    /    \::/    /                 \::::::::/    /     
+         |::::/    /               /:::/    /      \/____/                   \::::::/    /      
+         /:::/    /               /:::/    /                                  \::::/    /       
+        /:::/    /               /:::/    /                                    \::/____/        
+        \::/    /                \::/    /                                      ~~              
+         \/____/                  \/____/                                                       
+                                                                                                )"<<endl;
+    break;
+
+
+
+    }
+
+
+
+}
+
+
+bool CStringCompare(const char C1[N],const char C2[N]){
+    int i = 0;
+    while(C1[i]!='\0'&&C2[i]!='\0'){
+        if(C1[i]!=C2[i]){
+            return false;
+        }
+        i++;
+    }
+    return (C1[i]=='\0'&&C2[i]=='\0');
+}
+
+
+
+
+Account* AccountSearch(const char Name[N],AccountNode* head){
+    if(head==nullptr)
+        return nullptr;
+    
+    while(head!=nullptr){
+        if(CStringCompare(Name,head->data->Name)){
+            return head->data;
+        }
+        head = head->next;
+    }
+    return nullptr;
+}
+
+Account* AccountSearch(const int Id,AccountNode* head){
+    if(head==nullptr)
+        return nullptr;
+    while(head!=nullptr){
+        if(head->data->Id == Id){
+            return head->data;
+        }
+        head = head->next;
+    }
+    return nullptr;
+}
+
+bool Validation(char pass[N]){
+    return true;
+}
+
+bool Removal(int i){
+    switch(i){
+        case 1:
+
+
+            break;
+        case 2: //Delete all List
+            RHelp(head);
+            head = nullptr;
+            Session = nullptr;
+
+            break;
+
+        default:
+            return false;
+    }
+    return false;
+}
+
+void RHelp(AccountNode* temp){
+    if(temp==nullptr)
+        return;
+    RHelp(temp->next);
+    delete temp->data;
+    delete temp;
+}
+
+
+void Storage(int p){
+    switch(p){
+        case 1:
+
+            break;
+
+
+    }
+}
+
+void Acc(int i){
+    switch(i){
+        case 1:
+            bool flag = false;
+            char temp[N];
+            char pass[N];
+
+            cout<<"Creating Your First Account!"<<endl;
+            cout<<"Please Type Your Desired Name!"<<endl;
+            //Check if it shares the same name using another function, just make a searching algo,done
+            cin.getline(temp,N);
+            while (AccountSearch(temp, head) != nullptr) {
+                cout << "Account Name has been used! Please try again!" << endl;
+                cin.getline(temp, N);
+            }
+            cout<<"Please Type Your Desired Password!"<<endl;
+            cin.getline(pass,N);
+            while(!Validation(pass)){
+                cout<<"Invalid PassWord Input,Please try again!"<<endl;
+                cin.getline(pass,N);
+            }
+
+            AccountNode* current = head;
+            if(current == nullptr){
+                Account* nwAc = new Account(temp,pass,++AccountNo);
+                AccountNode* nwNode = new AccountNode(nwAc,nullptr,nullptr);
+                head = nwNode;
+            }else{
+                while(current->next!=nullptr){
+                    current = current->next;
+                }
+                Account* nwAc = new Account(temp,pass,++AccountNo);
+                AccountNode* nwNode = new AccountNode(nwAc,nullptr,current);
+                current->next = nwNode;
+            }
+            break;
+
+    }
+}
+
+void Login(){
+    char temp[N];
+    char pass[N];
+    int x = -1;
+
+    cout<<"Welcome to the Login System"<<endl;
+    cout<<"1.Login\n2.ResetMyPassword"<<endl;
+    cin>>x;
+    while(x!=1&&x!=2){
+        cout<<"Invalid Input,Please Retry"<<endl;
+        cin>>x;
+    }
+    if(x==1){
+        int count = 0;
+        cout<<"Enter Your Login Name"<<endl;
+        cin.ignore();
+        cin.getline(temp,N);
+        Account* check = AccountSearch(temp,head);
+        while(count<3&&check==nullptr){
+            cout<<"Invalid Name Please Retry"<<endl;
+            cin.getline(temp,N);
+            check = AccountSearch(temp,head);
+            ++count;
+            if(count==3){
+                cout<<"Please Try again later"<<endl;
+                return;
+            }
+        }
+        if(check!=nullptr){
+            count = 0;
+            cout<<"Please Enter your Password! "<<temp<<endl;
+            cin.getline(pass,N);
+            while(!CStringCompare(pass,check->password)&&count<3){
+                cout<<"Wrong Entry!"<<endl;
+                cin.getline(pass,N);
+                ++count;
+                if(count==3){
+                    cout<<"Please Try again later"<<endl;
+                    return;
+                }
+            }
+            cout<<"Welcome Back! "<<temp<<endl;
+            Session = check;
+        }                           //defensive only
+    }else if (x==2){
+        return;
+
+
+    }
+}
+
+
+
+
+
+void menu(){
+
+
+
+}
+
+
+int main(){
+    print(1);
+    Acc(1);
+    Login();
+    Removal(2);
+}
